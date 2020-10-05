a3fa169ae0b0dbdaab49d2152807b9bb906eda99
magiccoder890 <23fzhu@athenian.org>
2020-10-05 02:57:10 +0000

    This is Bert's homework

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,30 +1,79 @@
 public class MyMain {
 
-    // Calculate the probability of rolling at least one 6 when rolling 
-    // six dice. Uses 10000 trials.
-    // Returns in the answer as a double corresponding to the percentage
-    // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double six_posibility = 0;
+        double dice = 0;
+        int total = 0;
+        for (int u = 0;u < 9999; u++){
+            for (int i = 0;i < 6;i ++){
+                dice =  (int)(Math.random() * 6 + 1);
+                if (dice == 6){
+                    total += 1;
+                    break;
+                }
+                else {
+                    total += 0;
+            }
+        }
+        }
+
+        six_posibility = (double)total / 100;
+        return six_posibility;
     }
 
-    // Calculate the probability of rolling at least two 6's when rolling 
-    // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double six_posibility = 0;
+        double dice = 0;
+        int total = 0;
+        int times = 0;
+        for (int u = 0;u < 9999; u++){
+            for (int i = 0;i < 12;i ++){
+                dice =  (int)(Math.random() * 6 + 1);
+                if (dice == 6){
+                    times += 1;
+                    if (times == 2) {
+                        total += 1;
+                        break;
+                    }
+                }
+                else {
+                    total += 0;
+                }
+            }
+            times = 0;
+        }
+        six_posibility = (double)total / 100;
+        return six_posibility;
     }
 
-    // Calculate the probability of rolling at least three 6's when rolling 
-    // eighteen dice. Uses 10000 trials.
-    public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+    public static double probabilityThreeSixes(){
+        double six_posibility = 0;
+        double dice = 0;
+        int total = 0;
+        int times = 0;
+        for (int u = 0;u < 9999; u++){
+            for (int i = 0;i < 18;i ++){
+                dice =  (int)(Math.random() * 6 + 1);
+                if (dice == 6){
+                    times += 1;
+                    if(times == 3){
+                        total += 1;
+                        break;
+                    }
+                }
+                else {
+                    total += 0;
+                }
+            }
+            times = 0;
+        }
+
+        six_posibility = (double)total / 100;
+        return six_posibility;
     }
-
-
-    public static void main(String[] args) {
-        // YOUR CODE HERE
+    public static void main(String arg[]){
+    probabilityOneSix();
+    probabilityTwoSixes();
+    probabilityThreeSixes();
     }
 }
